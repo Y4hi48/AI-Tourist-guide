288e6d684fcedcac8702499f74bdaff0f9f8d373
Y4hi48 <115740749+Y4hi48@users.noreply.github.com>
2024-04-26 16:15:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,25 +6,22 @@ from streamlit_option_menu import option_menu
 
 from gemini_utility import (load_gemini_pro_model,
                             gemini_pro_response,
-                            gemini_pro_vision_response,
-                            embeddings_model_response)
-
+                            gemini_pro_vision_response)
 
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
 st.set_page_config(
-    page_title="Gemini AI",
+    page_title="Tourist Guide AI",
     page_icon="🧠",
     layout="centered",
 )
 
 with st.sidebar:
-    selected = option_menu('Gemini AI',
+    selected = option_menu('Tourist Guide AI',
                            ['ChatBot',
-                            'Image Captioning',
-                            'Embed text',
-                            'Ask me anything'],
-                           menu_icon='robot', icons=['chat-dots-fill', 'image-fill', 'textarea-t', 'patch-question-fill'],
+                            'Monument Detailing',
+                            'Historic Info'],
+                           menu_icon='globe2', icons=['chat-dots-fill', 'image-fill', 'patch-question-fill'],
                            default_index=0
                            )
 
@@ -41,40 +38,33 @@ def translate_role_for_streamlit(user_role):
 if selected == 'ChatBot':
     model = load_gemini_pro_model()
 
-    # Initialize chat session in Streamlit if not already present
-    if "chat_session" not in st.session_state:  # Renamed for clarity
+    if "chat_session" not in st.session_state:  
         st.session_state.chat_session = model.start_chat(history=[])
 
-    # Display the chatbot's title on the page
-    st.title("🤖 ChatBot")
+    st.title("🤖 Tourist Assistant")
 
-    # Display the chat history
     for message in st.session_state.chat_session.history:
         with st.chat_message(translate_role_for_streamlit(message.role)):
             st.markdown(message.parts[0].text)
 
-    # Input field for user's message
-    user_prompt = st.chat_input("Ask Gemini-Pro...")  # Renamed for clarity
+    user_prompt = st.chat_input("Ask Tourist Guide")  
     if user_prompt:
-        # Add user's message to chat and display it
         st.chat_message("user").markdown(user_prompt)
 
-        # Send user's message to Gemini-Pro and get the response
-        gemini_response = st.session_state.chat_session.send_message(user_prompt)  # Renamed for clarity
+        gemini_response = st.session_state.chat_session.send_message(user_prompt)  
 
-        # Display Gemini-Pro's response
         with st.chat_message("assistant"):
             st.markdown(gemini_response.text)
 
 
 # Image captioning page
-if selected == "Image Captioning":
+if selected == "Monument Detailing":
 
-    st.title("📷 Snap Narrate")
+    st.title("📷 Monument Detailing")
 
-    uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+    uploaded_image = st.file_uploader("Upload an image of a monument...", type=["jpg", "jpeg", "png"])
 
-    if st.button("Generate Caption"):
+    if st.button("Generate Description"):
         image = Image.open(uploaded_image)
 
         col1, col2 = st.columns(2)
@@ -83,9 +73,8 @@ if selected == "Image Captioning":
             resized_img = image.resize((800, 500))
             st.image(resized_img)
 
-        default_prompt = "write a short caption for this image"  # change this prompt as per your requirement
+        default_prompt = "describe this monument for a tourist"
 
-        # get the caption of the image from the gemini-pro-vision LLM
         caption = gemini_pro_vision_response(default_prompt, image)
 
         with col2:
@@ -93,25 +82,11 @@ if selected == "Image Captioning":
 
 
 # text embedding model
-if selected == "Embed text":
+if selected == "Historic Info":
 
-    st.title("🔡 Embed Text")
+    st.title("🔍 Historic Info")
 
-    # text box to enter prompt
-    user_prompt = st.text_area(label='', placeholder="Enter the text to get embeddings")
-
-    if st.button("Get Response"):
-        response = embeddings_model_response(user_prompt)
-        st.markdown(response)
-
-
-# text embedding model
-if selected == "Ask me anything":
-
-    st.title("❓ Ask me a question")
-
-    # text box to enter prompt
-    user_prompt = st.text_area(label='', placeholder="Ask me anything...")
+    user_prompt = st.text_area(label='', placeholder="Ask a historic question...")
 
     if st.button("Get Response"):
         response = gemini_pro_response(user_prompt)
